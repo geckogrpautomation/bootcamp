f7796e7dd3f081703775eb3661d0803dfa6b5bb3
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-09 16:58:51 +1100

    C

--- PreWork/javascript.js
+++ PreWork/javascript.js
@@ -5,6 +5,23 @@ function Grow(){
      
         } 
         
-
+function Blue(){  
+    
+        Mod6_2.getElementById("Mod6_2.box").style.backgroundcolor = "#ba3131";
+                 
+                 }
+        
+function Fade(){  
+            
+        Mod6_2.getElementById("Mod6_2.box").style.height = "50px";
+                     
+                }
+        
+function Reset(){  
+            
+        Mod6_2.getElementById("Mod6_2.box").style.height = "150px";
+        Mod6_2.getElementById("Mod6_2.box").style.backgroundcolor = "Orange";
+                         
+                }
 
         
