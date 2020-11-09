933de5e78680fe591fc9cd69402ee0a661b4af0d
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-09 14:26:10 +1100

    C

---
+++ PreWork/javascript.js
@@ -0,0 +1,10 @@
+
+// Grow Pushbutton OnClick fuction //
+document.getElementById('id').onclick = function() {
+
+    document.getElementById("box").style.height = "250px";
+};{
+   
+
+
+// Grow Pushbutton OnClick function //
