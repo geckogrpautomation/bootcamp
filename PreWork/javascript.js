9cdbc5cbb17cb8b4f143626dc9a3896fb699a639
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-09 16:56:53 +1100

    C

--- PreWork/javascript.js
+++ PreWork/javascript.js
@@ -1,15 +1,10 @@
 
-
-    Mod6_2.getElementById("Mod6_2.Button1").addEventListener("click", function(){
-
-        Mod6_2.getElementById("Mod6_2.box").style.height = "250px";
-     Mod6_2.getElementById()
-    }); 
+function Grow(){  
+    
+        document.getElementById("box").style.height = "250px";
+     
+        } 
         
-
-
-
-
 
 
         
