e3893c42c0f188ae48783cc9cb10241581c4d424
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-09 17:40:42 +1100

    C

---
+++ javascript.js
@@ -0,0 +1,28 @@
+
+//Grow button function
+Function Grow(){
+
+        document.getElementById("box").style.height = "250px";
+       
+    }
+       
+//Blue button function
+Function Blue(){
+    
+        document.getElementById("box").style.backgroundcolor = "#ba3131";
+      
+    } 
+//Fade button function
+Function Fade(){
+    
+        document.getElementById("box").style.backgroundcolor = "#ba3131";
+    
+    } 
+    
+//Fade button function
+Function Reset(){
+    
+        document.getElementById("box").style.height = "150px";
+        document.getElementById("box").style.backgroundcolor = "Orange";
+    
+    } 
