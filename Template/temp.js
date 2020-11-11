d3aac453805fa22660b00228138248f80e35752e
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-12 08:12:19 +1100

    Added header semantic to Homework week1

--- Template/temp.js
+++ Template/temp.js
@@ -1,10 +1,10 @@
 function navpb (pageindex){
 
-    var navpage = new String("navpage");      
-    console.log(navpagetarg.concat("https://www.temp", navpage,pageindex, "com.au" ));
+    var navpage = new String("navpage.valueOf");      
+    console.log(navpage.concat("https://www.temp", navpage.valueOf(), pageindex.valueOf(), "com.au" ));
     var URL = "http://www.tutorialspoint.com";    
 
 
-    <a href= navpage >lachlan.cameron@geckogrp.com.au</a>
+    <a href= {URL} >lachlan.cameron@geckogrp.com.au</a>
    
 }
