e3f905ea7592e12c79ffb6a26556e99c13b55b3e
Lachlan Cameron <lachlan.cameron@geckogrp.com.au>
2020-11-11 20:22:48 +1100

    Gecko template nav bar

---
+++ Template/temp.js
@@ -0,0 +1,10 @@
+function navpb (pageindex){
+
+    var navpage = new String("navpage");      
+    console.log(navpagetarg.concat("https://www.temp", navpage,pageindex, "com.au" ));
+    var URL = "http://www.tutorialspoint.com";    
+
+
+    <a href= navpage >lachlan.cameron@geckogrp.com.au</a>
+   
+}
